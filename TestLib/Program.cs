c224351014e0c7e344063dfbf4003419389fcf9c
james15mokoena <jamesmokoena15@yahoo.com>
2025-11-03 01:33:03 +0200

    No major changes.

--- TestLib/Program.cs
+++ TestLib/Program.cs
@@ -5,7 +5,7 @@ AdjacencyMapGraph<string, int> countryMap = new(false);
 var v1 = countryMap.InsertVertex("Qalabotjha");
 var v2 = countryMap.InsertVertex("Villiers");
 var v3 = countryMap.InsertVertex("Frankfort");
-//var e1 = countryMap.InsertEdge(v1, v2, 10);
+var e1 = countryMap.InsertEdge(v1, v2, 10);
 
 Console.WriteLine(v1.GetElement());
 Console.WriteLine(v2.GetElement());
